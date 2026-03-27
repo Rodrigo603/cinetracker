882295180adf46ca155e255ebfb78889086999a8
Rodrigo Dantas <rodrigo6031@gmail.com>
2026-03-27 11:31:35 -0300

    Iniciando projeto e adicão da api do imdb

---
+++ src/main/java/com/bd/cinetracker/CinetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.bd.cinetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinetrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bd/cinetracker/CinetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd.cinetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
