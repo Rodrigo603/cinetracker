049b9b0cecdab281a8c78a106db39082421d3d0f
Brendalu2005 <brenda.luanacb@gmail.com>
2026-03-28 01:00:07 -0300

    adicionando filmes e editando tabela FILME

---
+++ src/main/resources/db/migration/V4__adicionando_filmes.sql
@@ -0,0 +1,61 @@
+INSERT INTO FILME (
+    ID_IMDB,
+    TITULO,
+    POSTER_URL,
+    DESCRICAO,
+    BILHETERIA,
+    DURACAO,
+    ANO_LANCAMENTO,
+    PAIS_ORIGEM,
+    NOTA_IMDB
+) VALUES
+
+('tt0377092', 'Mean Girls', 'https://m.media-amazon.com/images/M/MV5BMjE1MDQ4MjI1OV5BMl5BanBnXkFtZTcwNzcwODAzMw@@._V1_SX300.jpg', 'Cady Heron faz sucesso com As Poderosas, a panelinha de garotas mais populares de sua nova escola, até cometer o erro de se apaixonar por Aaron Samuels, o ex-namorado da abelha-rainha Regina George.', 86058055.0, 97, 2004, 'EUA', 7.1),
+
+('tt0499549', 'Avatar', 'https://m.media-amazon.com/images/M/MV5BMDEzMmQwZjctZWU2My00MWNlLWE0NjItMDJlYTRlNGJiZjcyXkEyXkFqcGc@._V1_SX300.jpg', 'Um fuzileiro naval paraplégico enviado à lua Pandora em uma missão única fica dividido entre seguir suas ordens e proteger o mundo que ele sente ser seu lar.', 785221649.0, 162, 2009, 'EUA', 7.9),
+
+('tt18925334', 'Pearl', 'https://m.media-amazon.com/images/M/MV5BNzk0Y2RlZjItZDUzYi00Y2JhLTk5MzMtNjgzYWFmZmUxN2FmXkEyXkFqcGc@._V1_SX300.jpg', 'Em 1918, uma jovem à beira da loucura busca o estrelato em uma tentativa desesperada de escapar da monotonia, do isolamento e da falta de amor da vida na fazenda de seus pais.', 9423445.0, 103, 2022, 'EUA', 7.0),
+
+('tt14205554', 'KPop Demon Hunters', 'https://m.media-amazon.com/images/M/MV5BNTBiYWJlMjQtOTIyMy00NTY4LWFhOWItOWZhNzc3NGMyMjc2XkEyXkFqcGc@._V1_SX300.jpg', 'Um grupo de garotas de K-Pop de renome mundial equilibra suas vidas nos holofotes com suas identidades secretas como caçadoras de demônios.', 24300000.0, 95, 2025, 'EUA', 7.5),
+
+('tt13238346', 'Past Lives', 'https://m.media-amazon.com/images/M/MV5BYjQyMTNhNjUtN2VmYy00NWRhLTkwOTctMGVmNTBmNDIxYjZhXkEyXkFqcGc@._V1_SX300.jpg', 'Nora e Hae Sung, dois amigos de infância profundamente conectados, são separados depois que a família de Nora emigra da Coreia do Sul. Vinte anos depois, eles se reencontram para uma semana fatídica enquanto confrontam noções de amor e destino.', 11331983.0, 105, 2023, 'EUA', 7.8),
+
+('tt28156629', 'Eu Ainda Estou Aqui', 'https://m.media-amazon.com/images/M/MV5BZjdiNmNhYWItMWQ3MS00ZWQ5LWFkNzQtMmQ0NGUyZWQ4YWExXkEyXkFqcGdeQXVyMTM3NjgwMzIy._V1_SX300.jpg', 'Sinopse não disponível.', NULL, 72, 2023, 'Brasil', NULL),
+
+('tt0097165', 'Dead Poets Society', 'https://m.media-amazon.com/images/M/MV5BMDYwNGVlY2ItMWYxMS00YjZiLWE5MTAtYWM5NWQ2ZWJjY2Q3XkEyXkFqcGc@._V1_SX300.jpg', 'O professor rebelde John Keating retorna em 1959 para o prestigiado internato para meninos da Nova Inglaterra onde ele já foi um aluno estrela, usando poesia para encorajar seus pupilos a alcançarem novos patamares de autoexpressão.', 95860116.0, 128, 1989, 'EUA', 8.1),
+
+('tt0327597', 'Coraline', 'https://m.media-amazon.com/images/M/MV5BMzQxNjM5NzkxNV5BMl5BanBnXkFtZTcwMzg5NDMwMg@@._V1_SX300.jpg', 'Ao vagar por sua velha e labiríntica casa em sua nova e entediante cidade, uma jovem descobre uma porta escondida para uma versão estranhamente idealizada de sua vida que parece boa demais para ser verdade.', 116896576.0, 100, 2009, 'EUA', 7.8),
+
+('tt0112697', 'Clueless', 'https://m.media-amazon.com/images/M/MV5BMTgwYjQ1NDktOTcwNi00MWZhLTliYWQtZDg5NjZhY2U5ZTNlXkEyXkFqcGc@._V1_SX300.jpg', 'Fútil, rica e socialmente bem-sucedida, Cher está no topo da hierarquia social de sua escola em Beverly Hills. Vendo-se como uma casamenteira, Cher primeiro convence dois professores a namorarem entre si.', 56631572.0, 97, 1995, 'EUA', 6.9),
+
+('tt0372784', 'Batman Begins', 'https://m.media-amazon.com/images/M/MV5BMzA2NDQzZDEtNDU5Ni00YTlkLTg2OWEtYmQwM2Y1YTBjMjFjXkEyXkFqcGc@._V1_SX300.jpg', 'Após testemunhar a morte de seus pais, o bilionário Bruce Wayne aprende a arte de lutar para confrontar a injustiça. Ao retornar para Gotham como Batman, ele deve deter uma sociedade secreta que pretende destruir a cidade.', 206863479.0, 140, 2005, 'EUA', 8.2),
+
+('tt18351128', 'The Amazing Spiderman 2 Webb Cut', 'https://m.media-amazon.com/images/M/MV5BNzI0MmQyMzYtZDAzNi00ZWZiLWFjMTgtNzQwOTRjYTFlM2Y3XkEyXkFqcGc@._V1_SX300.jpg', 'A versão estendida de O Espetacular Homem-Aranha 2 de 2014. Inclui cenas nunca antes vistas e tomadas alternativas do filme.', NULL, NULL, 2021, 'EUA', NULL),
+
+('tt0251127', 'How to Lose a Guy in 10 Days', 'https://m.media-amazon.com/images/M/MV5BMjE4NTA1NzExN15BMl5BanBnXkFtZTYwNjc3MjM3._V1_SX300.jpg', 'Um executivo de publicidade e galã, para ganhar uma grande campanha, aposta que consegue fazer uma mulher se apaixonar por ele em 10 dias.', 105813373.0, 116, 2003, 'EUA', 6.5),
+
+('tt0096283', 'My Neighbor Totoro', 'https://m.media-amazon.com/images/M/MV5BYwm3MDE3YjEtMzIzZC00ODE5LTgxNTItNmUyMTBkM2M2NmNiXkEyXkFqcGc@._V1_SX300.jpg', 'Quando duas irmãs se mudam para o campo para ficarem perto de sua mãe doente, elas vivem aventuras com os maravilhosos espíritos da floresta que vivem por perto.', 2250213.0, 86, 1988, 'Japão', 8.1),
+
+('tt0876563', 'Ponyo', 'https://m.media-amazon.com/images/M/MV5BZDkzMzQ5ZmQtOTA3MC00MjhiLTk5M2UtNzk0MjEzZmVjN2UxXkEyXkFqcGc@._V1_SX300.jpg', 'Um menino de cinco anos desenvolve um relacionamento com Ponyo, uma jovem princesa peixinho dourado que deseja se tornar humana depois de se apaixonar por ele.', 16543471.0, 101, 2008, 'Japão', 7.6),
+
+('tt1099212', 'Twilight', 'https://m.media-amazon.com/images/M/MV5BMTQ2NzUxMTAxN15BMl5BanBnXkFtZTcwMzEyMTIwMg@@._V1_SX300.jpg', 'Quando Bella Swan se muda para uma pequena cidade no Noroeste do Pacífico, ela se apaixona por Edward Cullen, um colega de classe misterioso que se revela um vampiro de 108 anos.', 195553813.0, 122, 2008, 'EUA', 5.4),
+
+('tt0293508', 'The Phantom of the Opera', 'https://m.media-amazon.com/images/M/MV5BNDczNzg4OTM3MV5BMl5BanBnXkFtZTcwOTQzMTEzMw@@._V1_SX300.jpg', 'Uma jovem soprano torna-se a obsessão de um gênio musical desfigurado e assassino que vive abaixo da Ópera de Paris.', 51293931.0, 143, 2004, 'EUA', 7.2),
+
+('tt0266697', 'Kill Bill: Vol. 1', 'https://m.media-amazon.com/images/M/MV5BZmMyYzJlZmYtY2I3NC00NjAyLTkyZWItZjdjZDI1YTYyYTEwXkEyXkFqcGc@._V1_SX300.jpg', 'Após acordar de um coma de quatro anos, uma ex-assassina vinga-se da equipe de assassinos que a traiu.', 70099045.0, 111, 2003, 'EUA', 8.2),
+
+('tt0947798', 'Black Swan', 'https://m.media-amazon.com/images/M/MV5BNzY2NzI4OTE5MF5BMl5BanBnXkFtZTcwMjMyNDY4Mw@@._V1_SX300.jpg', 'Nina é uma bailarina talentosa, mas instável, à beira do estrelato. Levada ao limite por seu diretor artístico e por uma rival sedutora, a noção de realidade de Nina escapa, mergulhando-a em um pesadelo acordado.', 107770258.0, 108, 2010, 'EUA', 8.0),
+
+('tt33549683', 'Love Untangled', 'https://m.media-amazon.com/images/M/MV5BMGRmYmQ0N2UtZTdlMS00Y2EyLTk4MjMtZGQ2OGQ1ZTMxZjEyXkEyXkFqcGc@._V1_SX300.jpg', 'Segue Park Se-ri, de 19 anos, enquanto ela planeja alisar seu cabelo perpetuamente crespo antes de fazer uma confissão que mudará sua vida, tudo isso enquanto se envolve com o estudante transferido Han Yun-seok.', NULL, 118, 2025, 'Coreia do Sul', 7.2),
+
+('tt5950044', 'Superman', 'https://m.media-amazon.com/images/M/MV5BOGMwZGJiM2EtMzEwZC00YTYzLWIxNzYtMmJmZWNlZjgxZTMwXkEyXkFqcGc@._V1_SX300.jpg', 'O Superman deve reconciliar sua herança alienígena de Krypton com sua criação humana como o repórter Clark Kent. Como a personificação da verdade, da justiça e do estilo americano, ele logo se vê em um mundo que vê esses valores como antiquados.', 354223803.0, 129, 2025, 'EUA', 7.0),
+
+('tt5052448', 'Get Out', 'https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_SX300.jpg', 'Um jovem afro-americano visita os pais de sua namorada branca no fim de semana, onde sua crescente inquietação sobre a recepção deles eventualmente atinge um ponto de ebulição.', 176196665.0, 104, 2017, 'EUA', 7.8),
+
+('tt1119646', 'The Hangover', 'https://m.media-amazon.com/images/M/MV5BNDI2MzBhNzgtOWYyOS00NDM2LWE0OGYtOGQ0M2FjMTI2NTllXkEyXkFqcGc@._V1_SX300.jpg', 'Três amigos acordam de uma despedida de solteiro em Las Vegas sem memória da noite anterior e com o noivo desaparecido. Eles devem percorrer a cidade para encontrar o amigo a tempo de seu casamento.', 277339746.0, 100, 2009, 'EUA', 7.7),
+
+('tt1375666', 'Inception', 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg', 'Um ladrão que rouba segredos corporativos através do uso de tecnologia de compartilhamento de sonhos recebe a tarefa inversa de plantar uma ideia na mente de um CEO, mas seu passado trágico pode condenar o projeto e sua equipe ao desastre.', 292587330.0, 148, 2010, 'EUA', 8.8),
+
+('tt6751668', 'Parasite', 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg', 'A ganância e a discriminação de classe ameaçam a recém-formada relação simbiótica entre a rica família Park e o clã Kim, que vive na pobreza.', 53369749.0, 132, 2019, 'Coreia do Sul', 8.5),
+
+('tt0101414', 'Beauty and the Beast', 'https://m.media-amazon.com/images/M/MV5BYTY3NDg2YzktYWFjZC00MTExLTlmZDctMGY3MWYzZTlkYTNiXkEyXkFqcGc@._V1_SX300.jpg', 'Um príncipe amaldiçoado a passar seus dias como um monstro hediondo parte em busca de recuperar sua humanidade ao conquistar o amor de uma jovem mulher.', 218967620.0, 84, 1991, 'EUA', 8.0);
